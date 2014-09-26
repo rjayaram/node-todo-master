b741b297fd305f3f9e25080fe8c704aafe4bbf26
rjayaram <rekha.jayaram@familyheritagelife.com>
2014-09-26 15:43:48 -0400

    first checkin
    first checkin

---
+++ config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+	// the database url to connect
+	//url : 'mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu'
+	//url : 'mongodb://127.0.0.1:27017/test'
+	  url: 'mongodb://test:test@ds037990.mongolab.com:37990/lab'
+	
+	//Created an account on mongolab https://mongolab.com/databases/lab#users , username = rexxja1, password = (normal one), created a database lab
+	//setup a user test, test  for the database
+	//mongo tells you the connection uri that you can use to connect to the remote database
+	
+	 //mongodb://test:test@ds037990.mongolab.com:37990/lab
+	
+}
